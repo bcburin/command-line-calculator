5c7d74edbf4e14914f0c9c2d5c9790823be80839
Bruno <cburin.bruno@gmail.com>
2022-04-30 17:08:26 -0300

    Added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+#include "SyntaxTree.h"
+#include "Operator.h"
+
+using namespace std;
+
+// function<string(Token*)> print_token = [](Token* token) { return token->str() + "  "; };
+
+int main(int argc, char **argv) {
+
+  // SyntaxTree stree(argv[1]);
+
+  // cout << stree.result()->value();
+
+  string input;
+
+  while(true) { 
+    cout << ">> ";
+    cin >> input;
+
+
+    if(input == "exit()") break;
+
+    SyntaxTree stree(input);
+    cout << "   " <<  stree.result()->value() << endl;
+
+  }
+
+  return 0;
+}
+
