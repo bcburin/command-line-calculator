47e59749e3807f1d34d3d10a6c144b7edb8ceece
Bruno <cburin.bruno@gmail.com>
2022-05-02 09:34:01 -0300

    Refactored interface

--- src/Interface.cpp
+++ src/Interface.cpp
@@ -1,4 +1,5 @@
 #include "Interface.h"
+#include "Operand.h"
 
 std::vector<Statement*> read_statemets(std::istream* is, std::ostream* os,  bool run, bool re_throw) {
   std::vector<Statement*> statement_list = {};
@@ -8,77 +9,55 @@ std::vector<Statement*> read_statemets(std::istream* is, std::ostream* os,  bool
   // Read input from istream until reaching eof or specified "end" line
   while ( std::getline(*is, line) && trim(line) != "end" ) {
     try {
-      bool display_result = true;
       // Trim line
       line = trim(line);
+      // Determine statement
+      Statement* statement = nullptr;
       // Check for null input
       if (line == "") {}
-      // Check for trailing semicolon and set whether to display result
-      else if(line.back()==';') {
-        line = line.substr(0,line.size()-1);
-        display_result = false;
-      }
       // Check for if statement
       else if(line.find("if ")==0) {
         // Read nested statements
         std::vector<Statement*> nested_statements = read_statemets(is, os, false);
         // Create IfStatement object
-        auto statement = new IfStatement(line, nested_statements);
-        // Execute if in run mode
-        if (run) statement->execute();
-        // Save statement if not in run mode
-        if (!run) statement_list.push_back(statement);
+        statement = new IfStatement(line, nested_statements);
       } 
       // Check for while statement
       else if(line.find("while ")==0) {
         // Read nested statements
         std::vector<Statement*> nested_statements = read_statemets(is, os, false);
         // Create WhileStatement object
-        auto statement = new WhileStatement(line, nested_statements);
-        // Execute if in run mode
-        if (run) statement->execute();
-        // Save statement if not in run mode
-        if (!run) statement_list.push_back(statement);
+        statement = new WhileStatement(line, nested_statements);
       }
       // Check for "for" operator
       else if(line.find("for ")==0) {
         // Read nested statements
         std::vector<Statement*> nested_statements = read_statemets(is, os, false);
         // Create ForStatement object
-        auto statement = new ForStatement(line, nested_statements);
-        // Execute if in run mode
-        if (run) statement->execute();
-        // Save statement if not in run mode
-        if (!run) statement_list.push_back(statement);
+        statement = new ForStatement(line, nested_statements);
       }
       // Check for delete statement
       else if(line.find("delete ")==0) {
-        // Create object
-        auto statement = new DeleteStatement(line);
-        // Execute if in run mode
-        if (run) statement->execute();
-        // Save statement if not in run mode
-        if (!run) statement_list.push_back(statement);
+        statement = new DeleteStatement(line);
       }
       // Check for run statement
       else if(line.find("run ")==0) {
-        // Create object
-        auto statement = new RunStatement(line);
-        // Execute if in run mode
-        if (run) statement->execute();
-        // Save statement if not in run mode
-        if (!run) statement_list.push_back(statement);
+        statement = new RunStatement(line);
+      }
+      // Check for print statement
+      else if(line.find("print ")==0) {
+        statement = new PrintStatement(line,os);
       }
       // In the default case, the statement is interpreted as expression
       else {
-        auto exp = new Expression(line);
-        statement_list.push_back(exp);
-        // If in run mode, evaluate expression and output result in the given ostream
-        if(run) { 
-          exp->execute();
-          if(os && display_result) *os << "   " << exp->result() << std::endl;
-        }
+        statement = new Expression(line, os, run);
       }
+
+      // Execute if in run mode
+      if (run) statement->execute();
+      // Save statement if not in run mode
+      if (!run) statement_list.push_back(statement);
+
     } catch (const std::invalid_argument& error) {
       if(os) *os << "   " << error.what() << std::endl;
       if(re_throw) throw error;
